fbf6cb6a68d729029b843dc23b265396d80cc080
Camilo Ordoñez <cronox20@gmail.com>
2024-04-08 13:28:13 -0400

    adding default photo after signup

--- signup.php
+++ signup.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     // Insert the user into the database
-    $stmt = $mysqli->prepare("INSERT INTO users (username, email, password, created_at) VALUES (?, ?, ?, NOW())");
+    $stmt = $mysqli->prepare("INSERT INTO users (username, email, password, created_at, profile_picture) VALUES (?, ?, ?, NOW(), './images/unknown.jpg')");
     $stmt->bind_param("sss", $username, $email, $hashed_password);
     if ($stmt->execute()) {
         // Registration successful, redirect to login page
